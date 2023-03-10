db4f8fd192228460f1544e93b61007670913fcc1
Thierry Castro <thierrycastro555@gmail.com>
2023-03-09 23:19:50 -0300

    chatBody criado

--- src/components/ChatBody/index.jsx
+++ src/components/ChatBody/index.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { db } from "../../services/firebase";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { Container } from "./style";
+import Message from "../Message";
 
-const ChatBot = () => {
-  return <div>Chatbot</div>;
+const ChatBot = ({ chatId }) => {
+  const [messagesRes] = useCollection(
+    db
+      .collection("chats")
+      .doc(chatId)
+      .collection("messages")
+      .orderBy("timestamp", "asc")
+  );
+
+  const refBody = useRef("");
+
+  useEffect(() => {
+    if (refBody.current.scrollHeight > refBody.current.offsetHeight) {
+      refBody.current.scrollTop =
+        refBody.current.scrollHeight - refBody.current.offsetHeight;
+    }
+  }, [messagesRes]);
+
+  return (
+    <Container ref={refBody}>
+      {messagesRes?.docs.map((message) => (
+        <Message
+          key={message.id}
+          user={message.data().user}
+          message={{
+            message: message.data().message,
+            timestamp: message.data().timestamp?.toDate().getTime(),
+          }}
+        />
+      ))}
+    </Container>
+  );
 };
 
 export default ChatBot;
---
+++ src/components/Message/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import * as C from "./styles";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../../services/firebase";
+
+const Message = ({ user, message }) => {
+  const [userLoggedIn] = useAuthState(auth);
+
+  return (
+    <C.Container>
+      <C.Line className={userLoggedIn?.email === user ? "me" : ""}>
+        <C.Content>
+          <C.Message>{message.message}</C.Message>
+          <C.MessageData>
+            {new Date(message?.timestamp).toLocaleString()}
+          </C.MessageData>
+        </C.Content>
+      </C.Line>
+    </C.Container>
+  );
+};
